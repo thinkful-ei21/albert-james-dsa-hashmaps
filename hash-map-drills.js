816458da0bec4423bad8a098bbaa50b4d0f80c86
Albert Sare <thesundaymailman@gmail.com>
2018-07-30 11:08:43 -0700

    created lord of the rings character list; fixed length increment in HashMap class implementation

--- hash-map-drills.js
+++ hash-map-drills.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const HashMap = require('./hash-map.js');
+
+const lotr = new HashMap();
+
+const data = [
+  {Hobbit: 'Bilbo'},
+  {Hobbit: 'Frodo'},
+  {Wizard: 'Gandolf'},
+  {Human: 'Aragon'},
+  {Elf: 'Legolas'},
+  {Maiar: 'The Necromancer'},
+  {Maiar: 'Sauron'},
+  {RingBearer: 'Gollum'},
+  {LadyOfLight: 'Galadriel'},
+  {HalfElven: 'Arwen'},
+  {Ent: 'Treebeard'},
+];
+
+data.forEach(each => {
+  const key = Object.keys(each);
+  lotr.set(key[0], each[key[0]]);
+});
+
+console.log(lotr);
--- hash-map.js
+++ hash-map.js
@@ -1,84 +1,92 @@
 'use strict';
 
 class HashMap {
-    constructor(initialCapacity = 8) {
-        this.length = 0;
-        this._slots = [];
-        this._capacity = initialCapacity;
-        this._deleted = 0;
+  constructor(initialCapacity = 8) {
+    this.length = 0;
+    this._slots = [];
+    this._capacity = initialCapacity;
+    this._deleted = 0;
+  }
+
+  get(key) {
+    const index = this._findSlot(key);
+    if (this._slots[index] === undefined) {
+      throw new Error('Key error');
+    }
+    return this._slots[index].value;
+  }
+
+  set(key, value) {
+    const loadRatio = (this.length + this._deleted + 1) / this._capacity;
+    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+      this._resize(this._capacity * HashMap.SIZE_RATIO);
     }
 
-    get(key) {
-        const index = this._findSlot(key);
-        if (this._slots[index] === undefined) {
-            throw new Error('Key error');
-        }
-        return this._slots[index].value;
+    const index = this._findSlot(key);
+    // check to see if this._slots already has an item, if yes, then it overrides previous value, so length does not increase
+    if (!this._slots[index]) {
+      this.length++;
     }
+    // assign this._slots[index] with key and value, so if key already exists, it wil still be the same and only value is overridden
+    this._slots[index] = {
+      key,
+      value,
+      deleted: false
+    };
+  }
 
-    set(key, value) {
-        const loadRatio = (this.length + this._deleted + 1) / this._capacity;
-        if (loadRatio > HashMap.MAX_LOAD_RATIO) {
-            this._resize(this._capacity * HashMap.SIZE_RATIO);
-        }
-
-        const index = this._findSlot(key);
-        this._slots[index] = {
-            key,
-            value,
-            deleted: false
-        };
-        this.length++;
+  remove(key) {
+    const index = this._findSlot(key);
+    const slot = this._slots[index];
+    if (slot === undefined) {
+      throw new Error('Key error');
     }
+    slot.deleted = true;
+    this.length--;
+    this._deleted++;
+  }
 
-    remove(key) {
-        const index = this._findSlot(key);
-        const slot = this._slots[index];
-        if (slot === undefined) {
-            throw new Error('Key error');
-        }
-        slot.deleted = true;
-        this.length--;
-        this._deleted++;
+  _findSlot(key) {
+    const hash = HashMap._hashString(key);
+    const start = hash % this._capacity;
+
+    for (let i = start; i < start + this._capacity; i++) {
+      // if this loop goes pass this._capacity, next line will bring the loop to the beginning of the index
+      const index = i % this._capacity;
+      // grab the current slot
+      const slot = this._slots[index];
+      if (slot === undefined || (slot.key === key && !slot.deleted)) {
+        return index;
+      }
     }
+  }
 
-    _findSlot(key) {
-        const hash = HashMap._hashString(key);
-        const start = hash % this._capacity;
+  _resize(size) {
+    const oldSlots = this._slots;
+    this._capacity = size;
+    // Reset the length - it will get rebuilt as you add the items back
+    this.length = 0;
+    this._deleted = 0;
+    this._slots = [];
 
-        for (let i = start; i < start + this._capacity; i++) {
-            const index = i % this._capacity;
-            const slot = this._slots[index];
-            if (slot === undefined || (slot.key == key && !slot.deleted)) {
-                return index;
-            }
-        }
+    for (const slot of oldSlots) {
+      if (slot !== undefined && !slot.deleted) {
+        this.set(slot.key, slot.value);
+      }
     }
+  }
 
-    _resize(size) {
-        const oldSlots = this._slots;
-        this._capacity = size;
-        // Reset the length - it will get rebuilt as you add the items back
-        this.length = 0;
-        this._deleted = 0;
-        this._slots = [];
-
-        for (const slot of oldSlots) {
-            if (slot !== undefined && !slot.deleted) {
-                this.set(slot.key, slot.value);
-            }
-        }
+  static _hashString(string) {
+    let hash = 5381;
+    for (let i = 0; i < string.length; i++) {
+      hash = (hash << 5) + hash + string.charCodeAt(i);
+      hash = hash & hash;
     }
-
-    static _hashString(string) {
-        let hash = 5381;
-        for (let i = 0; i < string.length; i++) {
-            hash = (hash << 5) + hash + string.charCodeAt(i);
-            hash = hash & hash;
-        }
-        return hash >>> 0;
-    }
+    return hash >>> 0;
+  }
 }
 
 HashMap.MAX_LOAD_RATIO = 0.9;
 HashMap.SIZE_RATIO = 3;
+
+module.exports = HashMap;
